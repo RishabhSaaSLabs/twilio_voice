89decf56a2641d0f955e243b036b5ac2186d21c8
Rishabh Rawat <rishabh@saaslabs.co>
2024-10-25 12:18:26 +0530

    handled missed call notification correctly

--- ios/Classes/SwiftTwilioVoicePlugin.swift
+++ ios/Classes/SwiftTwilioVoicePlugin.swift
@@ -612,7 +612,7 @@ public class SwiftTwilioVoicePlugin: NSObject, FlutterPlugin,  FlutterStreamHand
             let title = userName ?? self.clients["defaultCaller"] ?? self.defaultCaller
             content.title = "Helpwise Missed Call";
             content.body = "Missed call from "+title;
-            content.sound = UNNotificationSound.default()
+            content.sound = UNNotificationSound.default;
 
             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
             let request = UNNotificationRequest(identifier: UUID().uuidString,
